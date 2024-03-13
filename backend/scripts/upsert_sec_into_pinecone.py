dd444b920f12f3a1d2b764baf08c5dc78ade8825
Helen <helkochetkova@gmail.com>
2024-03-13 16:45:11 -0400

    added sec filings and pinecone scripts)

---
+++ backend/scripts/upsert_sec_into_pinecone.py
@@ -0,0 +1,25 @@
+import os
+
+from pinecone import Pinecone
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+from llama_index.embeddings.voyageai import VoyageEmbedding
+
+model_name = "voyage-lite-02-instruct"
+voyage_api_key = os.environ.get("VOYAGE_API_KEY")
+
+embed_model = VoyageEmbedding(
+    model_name=model_name,
+    voyage_api_key=voyage_api_key,
+)
+
+embeddings = embed_model.get_query_embedding("instruct")
+
+documents = SimpleDirectoryReader("data/sec-edgar-filings").load_data()
+
+
+pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
+pinecone_index = pc.Index("sec")
+vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
