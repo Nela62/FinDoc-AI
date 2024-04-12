613d6efdae51f2ad7208ea07e53bb9b265a0b527
Helen <helkochetkova@gmail.com>
2024-04-11 21:09:08 -0400

    added textmenu to the top

--- frontend/src/app/reports/[report]/page.tsx
+++ frontend/src/app/reports/[report]/page.tsx
@@ -34,6 +34,7 @@ export type AiState = {
 };
 
 import { DocumentType } from '@/stores/documents-store';
+import { EditorToolbar } from '@/components/Toolbar';
 
 const generateReport = async (ticker: string, editor: Editor) => {
   const company = tickers.find((t) => t.value === ticker)?.label;
@@ -406,30 +407,32 @@ export default function Report({ params }: { params: { report: string } }) {
             </div>
           </div>
         ) : (
-          <div className="relative flex flex-col flex-1 h-full overflow-hidden ">
-            {/* TODO: Table of contents */}
-            <ScrollArea.Root className="overflow-hidden h-full w-full pt-3 px-1">
-              <ScrollArea.Viewport
-                style={{ boxShadow: '0px 1px 2px 0px rgba(0,0,0,0.2)' }}
-                className="h-full bg-white w-full rounded-t-[12px] border-zinc-300 border-[0.5px]"
-              >
-                <EditorContent editor={editor} className="flex-1" />
-              </ScrollArea.Viewport>
-              <ScrollArea.Scrollbar
-                className="flex select-none touch-none p-0.5 mt-3.5 mr-0.5 transition-colors duration-[160ms] ease-out data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"
-                orientation="vertical"
-              >
-                <ScrollArea.Thumb className="flex-1 bg-gray-400 hover:bg-gray-500 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
-              </ScrollArea.Scrollbar>
-            </ScrollArea.Root>
+          <div className="pt-3 px-1 flex-1">
+            <div className="relative flex flex-col h-full overflow-hidden rounded-t-[12px] shadow-finpanel bg-white border-zinc-300 border-[0.5px]">
+              <div className="h-[44px] w-full">
+                <EditorToolbar editor={editor} />
+              </div>
+              {/* TODO: Table of contents */}
+              <ScrollArea.Root className="overflow-hidden h-full w-full ">
+                <ScrollArea.Viewport className="h-full w-full">
+                  <EditorContent editor={editor} className="flex-1" />
+                </ScrollArea.Viewport>
+                <ScrollArea.Scrollbar
+                  className="flex select-none touch-none p-0.5 mt-3.5 mr-0.5 transition-colors duration-[160ms] ease-out data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"
+                  orientation="vertical"
+                >
+                  <ScrollArea.Thumb className="flex-1 bg-gray-400 hover:bg-gray-500 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]" />
+                </ScrollArea.Scrollbar>
+              </ScrollArea.Root>
 
-            <ContentItemMenu editor={editor} />
-            <LinkMenu editor={editor} appendTo={menuContainerRef} />
-            <TextMenu editor={editor} />
-            <ColumnsMenu editor={editor} appendTo={menuContainerRef} />
-            <TableRowMenu editor={editor} appendTo={menuContainerRef} />
-            <TableColumnMenu editor={editor} appendTo={menuContainerRef} />
-            <ImageBlockMenu editor={editor} appendTo={menuContainerRef} />
+              <ContentItemMenu editor={editor} />
+              <LinkMenu editor={editor} appendTo={menuContainerRef} />
+              {/* <TextMenu editor={editor} /> */}
+              <ColumnsMenu editor={editor} appendTo={menuContainerRef} />
+              <TableRowMenu editor={editor} appendTo={menuContainerRef} />
+              <TableColumnMenu editor={editor} appendTo={menuContainerRef} />
+              <ImageBlockMenu editor={editor} appendTo={menuContainerRef} />
+            </div>
           </div>
         )}
         <RightSideBar />
