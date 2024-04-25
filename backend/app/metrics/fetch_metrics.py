531e8a2f5a7b1d21a669190723f5af099f5084e6
Helen <helkochetkova@gmail.com>
2024-04-25 15:14:10 -0400

    delete backend

--- backend/app/metrics/fetch_metrics.py
+++
@@ -1,8 +0,0 @@
-import requests
-from app.core.config import settings
-
-
-def fetch_metrics(company_ticker: str):
-    requests.get(
-        f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={company_ticker}&apikey={settings.ALPHA_VANTAGE_API_KEY}"
-    )
