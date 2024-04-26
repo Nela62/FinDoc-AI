85ed400d72c869f34372483cd9258a4417add79f
Helen <helkochetkova@gmail.com>
2024-04-26 14:37:49 -0400

    finished auth screen

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import { createClient } from '@/lib/supabase/server';
@@ -15,74 +14,63 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { LoginAuthForm, formType } from './components/LoginAuthForm';
+import { Cover } from './components/Cover';
+const signIn = async ({ email, password }: formType) => {
+  'use server';
 
-export default function Login() {
-  const signIn = async ({ email, password }: formType) => {
-    'use server';
+  const supabase = createClient();
 
-    const supabase = createClient();
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  return { error: error ? error.message : null };
+};
+export default async function Login() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-    return { error: error ? error.message : null };
-  };
-
-  const signUp = async (formData: FormData) => {
-    'use server';
-
-    const origin = headers().get('origin');
-    const email = formData.get('email') as string;
-    const password = formData.get('password') as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-
-    if (error) {
-      return redirect('/login?message=Could not authenticate user');
-    }
-
-    return redirect('/login?message=Check email to continue sign in process');
-  };
-  // TODO: add tanstack/react-query for auth and isloading
+  if (user) {
+    redirect('/reports/all');
+  }
 
   return (
-    <div className="mx-auto flex flex-col justify-center space-y-7 w-fit">
-      <div className="flex flex-col gap-2 w-fit mx-auto items-center">
-        <Image
-          src="/coreline_logo.png"
-          alt="Coreline logo"
-          className="h-7 w-7"
-          width={0}
-          height={0}
-          sizes="100vw"
-        />
-        <h2 className="text-3xl font-semibold text-foreground">Welcome back</h2>
+    <div className="container bg-background relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Cover />
+      <div className="mx-auto flex flex-col justify-center space-y-7 w-fit">
+        <div className="flex flex-col gap-2 w-fit mx-auto items-center">
+          <Image
+            src="/coreline_logo.png"
+            alt="Coreline logo"
+            className="h-7 w-7"
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+          <h2 className="text-3xl font-semibold text-foreground">
+            Welcome back
+          </h2>
+        </div>
+        <Card className="max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <LoginAuthForm formAction={signIn} />
+          </CardContent>
+          <CardFooter className="text-sm justify-center">
+            Don&apos;t have an account?{' '}
+            <Link href="/register" className="underline ml-1">
+              Sign up
+            </Link>
+          </CardFooter>
+        </Card>
       </div>
-      <Card className="max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <LoginAuthForm formAction={signIn} />
-        </CardContent>
-        <CardFooter className="text-sm justify-center">
-          Don&apos;t have an account?{' '}
-          <Link href="/register" className="underline ml-1">
-            Sign up
-          </Link>
-        </CardFooter>
-      </Card>
     </div>
   );
 }
--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -15,58 +15,69 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { RegisterAuthForm, formType } from './components/RegisterAuthForm';
+import { Cover } from './components/Cover';
 
-export default function Login() {
-  const signUp = async ({ email, password }: formType) => {
-    'use server';
+const signUp = async ({ email, password }: formType) => {
+  'use server';
 
-    const origin = headers().get('origin');
-    const supabase = createClient();
+  const origin = headers().get('origin');
+  const supabase = createClient();
 
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${origin}/auth/callback`,
+    },
+  });
 
-    return { error: error ? error.message : null };
-  };
-  // TODO: add tanstack/react-query for auth and isloading
+  return { error: error ? error.message : null };
+};
 
+export default async function Register() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect('/reports/all');
+  }
   return (
-    <div className="mx-auto flex flex-col justify-center space-y-7 w-fit">
-      <div className="flex flex-col gap-2 w-fit mx-auto items-center">
-        <Image
-          src="/coreline_logo.png"
-          alt="Coreline logo"
-          className="h-7 w-7"
-          width={0}
-          height={0}
-          sizes="100vw"
-        />
-        <h2 className="text-3xl font-semibold text-foreground">
-          Create a new account
-        </h2>
+    <div className="container bg-background relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Cover />
+      <div className="mx-auto flex flex-col justify-center space-y-7 w-fit">
+        <div className="flex flex-col gap-2 w-fit mx-auto items-center">
+          <Image
+            src="/coreline_logo.png"
+            alt="Coreline logo"
+            className="h-7 w-7"
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+          <h2 className="text-3xl font-semibold text-foreground">
+            Create a new account
+          </h2>
+        </div>
+        <Card className="max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Sign Up</CardTitle>
+            <CardDescription>
+              Enter your information to create an account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <RegisterAuthForm formAction={signUp} />
+          </CardContent>
+          <CardFooter className="text-sm justify-center">
+            Already have an account?{' '}
+            <Link href="/login" className="underline ml-1">
+              Login
+            </Link>
+          </CardFooter>
+        </Card>
       </div>
-      <Card className="max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-2xl">Sign Up</CardTitle>
-          <CardDescription>
-            Enter your information to create an account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <RegisterAuthForm formAction={signUp} />
-        </CardContent>
-        <CardFooter className="text-sm justify-center">
-          Already have an account?{' '}
-          <Link href="/login" className="underline ml-1">
-            Login
-          </Link>
-        </CardFooter>
-      </Card>
     </div>
   );
 }
