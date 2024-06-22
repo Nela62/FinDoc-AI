957864a9abd1fa972202aa818456ebbe77705d96
Helen Kochetkova <helen@finpanel.com>
2024-06-22 02:24:25 -0400

    added policies

---
+++ frontend/src/app/(policies)/privacy/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { TermlyScriptWrapper } from '../TermlyComponent';
+
+export default function Policy() {
+  return <TermlyScriptWrapper dataId="76e3f439-c2d2-433f-a792-632a9de12cdd" />;
+}
---
+++ frontend/src/app/(policies)/tos/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { TermlyScriptWrapper } from '../TermlyComponent';
+
+export default function Policy() {
+  return <TermlyScriptWrapper dataId="5da2b4a8-f3b9-46d7-8224-34971633f0f1" />;
+}
