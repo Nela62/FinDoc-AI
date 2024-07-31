6ae02eecb3f346cf4ce26575072784a4cc9873ab
Helen Kochetkova <helen@finpanel.com>
2024-07-30 22:57:05 -0400

    changed config

--- supabase/migrations/20240730020648_create_table_profiles.sql
+++ supabase/migrations/20240730020648_create_table_profiles.sql
@@ -2,9 +2,22 @@ ALTER TABLE public.subscriptions RENAME TO profiles;
 
 ALTER TABLE public.profiles
   ADD COLUMN name text,
-  ADD COLUMN email text UNIQUE NOT NULL,
+  ADD COLUMN email text,
   ADD COLUMN "role" text,
   ADD COLUMN "sec_filings_frequency" text;
+
+UPDATE
+  public.profiles
+SET
+  email = auth.users.email
+FROM
+  auth.users
+WHERE
+  public.profiles.id = auth.users.id;
+
+-- Add the UNIQUE constraint
+ALTER TABLE public.profiles
+  ADD CONSTRAINT profiles_email_unique UNIQUE (email);
 
 -- Drop trigger
 DROP TRIGGER on_auth_user_created ON auth.users;
